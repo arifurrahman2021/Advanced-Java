2b32dbe2c32c5a6a2f6538415da222d1545c4d89
Md. Arifur Rahman Alvi <156001005+arifurrahman2021@users.noreply.github.com>
2026-09-23 09:57:57 +0600

    Delete L10 serviceLayer jpaHibernate embadded elementCollection oneToOne directory

--- L10 serviceLayer jpaHibernate embadded elementCollection oneToOne/src/main/java/com/github/listdynamictable/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.listdynamictable;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
