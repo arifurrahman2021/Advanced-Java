5c8663af844fc26b6d66877830f97c5d3ac8be4e
Md. Arifur Rahman Alvi <YOUR_GITHUB_EMAIL>
2026-09-23 10:11:28 +0600

    Add Advanced Java L10

---
+++ L10 serviceLayer jpaHibernate embadded elementCollection oneToOne/src/main/java/com/github/listdynamictable/Address.java
@@ -0,0 +1,13 @@
+package com.github.listdynamictable;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Data
+@Embeddable
+public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+}
---
+++ L10 serviceLayer jpaHibernate embadded elementCollection oneToOne/src/main/java/com/github/listdynamictable/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.github.listdynamictable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
